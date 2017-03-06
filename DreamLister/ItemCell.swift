147c4e238a93751e24dccdf23462e850ecc7c84c
Justin Ward <justin@quietlike.ninja>
2017-03-06 19:01:53 +1000

    DreamLister Part 3

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Justin Ward on 6/3/17.
+//  Copyright © 2017 Justin Ward. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet weak var thumb: UIImageView!
+
+    @IBOutlet weak var title: UILabel!
+
+    @IBOutlet weak var price: UILabel!
+    
+    @IBOutlet weak var details: UILabel!
+}
