dda18649eb0512fdecdaba679f418943af56b28b
Justin Ward <justin@quietlike.ninja>
2017-03-07 22:45:24 +1000

    Part 10

--- DreamLister/ItemCell.swift
+++ DreamLister/ItemCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ItemCell: UITableViewCell {
-    @IBOutlet weak var thumb: UIImage!
+    @IBOutlet weak var thumb: UIImageView!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var details: UILabel!
@@ -19,6 +19,7 @@ class ItemCell: UITableViewCell {
         title.text = item.title
         price.text = "$\(item.price)"
         details.text = item.details
+        thumb.image = item.toImage?.image as? UIImage
         
     }
 }
